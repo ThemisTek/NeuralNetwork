14d57d9ae55218b3efe305e8f86e68a708de9d2a
ThemisTek <59509230+ThemisTek@users.noreply.github.com>
2020-03-24 18:23:42 +0200

    Update

--- networkClass.py
+++ networkClass.py
@@ -3,6 +3,11 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from cv2 import cv2
 import numpy as np
+import keras
+import tensorflow as tf
+
+def dimensions():
+    return [45,45]
 
 class Detector :
     def __init__(self,x,y,width,height,detected,imgName):
@@ -14,46 +19,54 @@ class Detector :
         self.detected = detected
 
 def NetworkModel():
-    img_width = 45
-    img_height = 45
+    d = dimensions()
+    img_width = d[0]
+    img_height = d[1]
 
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=( img_width, img_height,3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model = Sequential()
+    # model.add(Conv2D(32, (3, 3), input_shape=( img_width, img_height,3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(32, (3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(64, (3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(5))
-    model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+    # model.add(Dense(64))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(5))
+    # model.add(Activation('sigmoid'))
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(img_width, img_height,3)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(units = 5)
+    ])
+
+    model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
     return model
 
-def detector2Outut(detector):
+def detector2Output(detector):
     output = np.empty((5,1))
-    output[0]=detector.x
-    output[1]=detector.y
-    output[2]=detector.width
-    output[3]=detector.height
-    output[4]=detector.detected
+    output[0,0]=detector.x
+    output[1,0]=detector.y
+    output[2,0]=detector.width
+    output[3,0]=detector.height
+    output[4,0]=detector.detected
     return output
 
 
 def detector2Input(detector):
-    image = cv2.imread(detector.filename)
+    image = cv2.imread(detector.imgName)
     image = np.resize(image,(45,45,3))
     image = np.divide(image,255)
+    return image
 
 
--- networkTrainer.py
+++ networkTrainer.py
@@ -1,12 +1,22 @@
 import networkClass
 import numpy as np
 import cv2
+import keras
 from keras.preprocessing.image import ImageDataGenerator 
 from keras.models import Sequential 
 from keras.layers import Conv2D, MaxPooling2D 
 from keras.layers import Activation, Dropout, Flatten, Dense 
 from keras import backend as K 
 from networkClass import Detector
+import tensorflow as tf
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(type(train_labels),train_labels[0])
+
+print(train_images.shape,train_labels.shape)
 
 np_load_old = np.load
 
@@ -16,8 +26,32 @@ objectReader = np.load("test.npy")
 
 model = networkClass.NetworkModel()
 
+d = networkClass.dimensions()
+width = d[0]
+height = d[1]
+inputSize = len(objectReader)
+inputs=np.empty((inputSize,width,height,3))
+outputs = np.empty((inputSize,5,1))
+
+
+i=0
+
 for o in objectReader:
-    print(o.imgName)
+    outputs[i] = networkClass.detector2Output(o)
+    inputs[i] = networkClass.detector2Input(o)
+    i=i+1
+
+
+print(outputs.shape)
+print(inputs.shape)
+
+
+labels = tf.reshape(outputs, [10,5])
+
+for layer in model.layers:
+    print(layer.output_shape)
+
+model.fit(inputs, labels, steps_per_epoch=200,epochs = 10)
 
 
 
